82f382086ce4d0b178c6e90c4eb8688af3c61969
BastosC <bastien.chantrel.pro@gmail.com>
2022-06-23 19:33:11 +0200

    merge old templates with laravel routes

--- public/README.md
+++
@@ -1,7 +0,0 @@
-# eval-em-laravel-2022
-
-Evaluation sur 1 semaine, créer un mini site e-commerce avec Laravel
-
-# DEMO :collision: : [Not ready](https://www.google.com)
-
-## Brief :
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// ------------------------------------------ PUBLIC VIEWS
+
+// Home page
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Categories pages
+Route::get('/categories/soldes', function () {
+    return view('categories/soldes');
+});
+Route::get('/categories/homme', function () {
+    return view('categories/homme');
+});
+Route::get('/categories/femme', function () {
+    return view('categories/femme');
+});
+
+
+// Product page
+Route::get('/product/{id}', function ($id) {
+    echo $id;
+    return view('product/index');
+});
+
+
+// 404 Page
+Route::fallback(function () {
+    return view('404');
+
+});
+// ------------------------------------------ ADMIN VIEWS
+// Login page
+Route::get('/admin', function () {
+    return view('admin/index');
+});
+
+// Admin list products page
+Route::get('/admin/products', function () {
+    return view('admin/products');
+});
+// Admin list categories page
+Route::get('/admin/categories', function () {
+    return view('admin/categories');
+});
+
+
+
